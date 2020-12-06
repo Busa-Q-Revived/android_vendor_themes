3f67275ca9db5318d4a1f248e19159457880e390
703joko <703joko@gmail.com>
2020-12-07 04:53:02 +0700

    Add brightness, qsshape, panelbg style [3/3]

--- overlays.mk
+++ overlays.mk
@@ -162,3 +162,38 @@ PRODUCT_PACKAGES += \
     InsideWiFiOverlay \
     IosWiFiOverlay \
     BarsWiFiOverlay
+
+# Panel Background
+PRODUCT_PACKAGES += \
+    PanelBgBatik \
+    PanelBgKece \
+    PanelBgOutline
+
+# Qs shape
+PRODUCT_PACKAGES += \
+    QsShapeA \
+    QsShapeB \
+    QsShapeC \
+    QsShapeD \
+    QsShapeE \
+    QsShapeF \
+    QsShapeG \
+    QsShapeH \
+    QsShapeI \
+    QsShapeJ \
+    QsShapeK \
+    QsShapeL \
+    QsShapeM \
+    QsShapeN \
+    QsShapeO \
+    QsShapeP \
+    QsShapeQ \
+    QsShapeR \
+    QsShapeS
+
+# Brightness sliders Styles
+PRODUCT_PACKAGES += \
+    BrightnessTypeA \
+    BrightnessTypeB \
+    BrightnessTypeC \
+    BrightnessTypeD
